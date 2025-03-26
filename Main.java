766c3fe0039e3218a42f3d0239d196322c5a2fc0
Andrew-bot1 <andrewhayes248@gmail.com>
2025-03-26 13:59:58 -0500

    gr

--- Main.java
+++ Main.java
@@ -1,28 +1,40 @@
+
 import java.util.Scanner;  // Import the Scanner class
+import java.util.Random; 
+
+
+enum Choice {
+    ROCK,
+    PAPER,
+    SCISSORS
+  }    
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-        //ask for price of the item
-      System.out.println("What is the price of the item?");
-      //get input for item price
-      Double price = myObj.nextDouble();
 
-      //ask for sales tax
-      System.out.println("What is the sales tax of the item?");
-      //get input for tax
-      Double tax = myObj.nextDouble();
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        Random random = new Random();
+      System.out.println("Enter rock, paper, scissors, or exit");
+      String userInput = scanner.nextLine().toUpperCase();
 
-      //ask for discount of the item
-      System.out.println("What is the promotion discount of the item?");
-      //get input for item discount
-      Double discount = myObj.nextDouble();
+      Choice userChoice = Choice.valueOf(userInput);
 
-      //calculate total
-      double total = (price * discount) * (tax+1);
-      
-      //display total cost
-      System.out.println("The total cost of the item is " +total);
+      Choice computerChoice = Choice.values()[random.nextInt(3)];
+      System.out.println("Computer choice: " +computerChoice);
+
+      if (userChoice == computerChoice) {
+        System.out.println("It's a tie!");
+      }
+      else if ((userChoice == Choice.ROCK && computerChoice == Choice.SCISSORS) || 
+      (userChoice == Choice.PAPER && computerChoice == Choice.ROCK) ||
+      (userChoice == Choice.SCISSORS && computerChoice == Choice.PAPER)) {
+        System.out.println("You win!");
+      }
+      else if ((computerChoice == Choice.ROCK && userChoice == Choice.SCISSORS) || 
+      (computerChoice == Choice.PAPER && userChoice == Choice.ROCK) ||
+      (computerChoice == Choice.SCISSORS && userChoice == Choice.PAPER)) {
+        System.out.println("You lose!");
+      }
     }
   }
